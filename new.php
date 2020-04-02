7bbdae05f9e246e8e2c494d966c6a333dfcdacbd
dimm8187@gmail.com <dimm8187@gmail.com>
2020-04-01 21:30:55 -0400

    css1

--- new.php
+++ new.php
@@ -11,15 +11,11 @@
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                
+                            <p class="letter">Record successfully inserted</p>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Record successfully inserted</p>
-                        </div>
-                            
+                        </div>                          
                     </div>
                 </div>
             </div>    
