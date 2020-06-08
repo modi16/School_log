525841aed5697d4ad380ad68e0274848f0d41294
dimm8187@gmail.com <dimm8187@gmail.com>
2020-06-07 21:24:20 -0400

    css

--- new.php
+++ new.php
@@ -2,6 +2,7 @@
     <head>
         <link rel="stylesheet" type="text/css" href="home.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" >
+        <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>  
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
